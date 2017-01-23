41eb1c6259f950094deb15ac01e5916f3f8b7479
Kenneth Black and Marty Kovach <kenneth@>
2017-01-23 09:17:43 -0800

    link marketplace route to index.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    AboutComponent
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
     BrowserModule,
