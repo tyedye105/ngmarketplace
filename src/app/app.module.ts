e523ad1ed51685dafed3d22666b566529f58e7e5
Andrew Accuardi and Marty Kovach <marty@>
2017-01-24 09:18:43 -0800

    create album detail page, create dynamic routing for album details.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
-import { MarketplaceComponent } from './marketplace/marketplace.component'
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     AboutComponent,
-    MarketplaceComponent
+    MarketplaceComponent,
+    AlbumDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent} from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component'
 
 const appRoutes: Routes = [{
+
     path: '',
     component: WelcomeComponent
   }, {
@@ -13,6 +15,9 @@ const appRoutes: Routes = [{
   }, {
     path: 'marketplace',
     component: MarketplaceComponent
+  }, {
+    path: 'albums/:id',
+    component: AlbumDetailComponent
 }];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
