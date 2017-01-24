a89f47c475040aecf95361c6551f7f077e115220
Andrew Accuardi and Marty Kovach <andrew@>
2017-01-24 09:26:59 -0800

    add dynamic display for dynamic routes.

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Album } from '../album.model';
 
 @Component({
   selector: 'album-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-detail.component.css']
 })
 export class AlbumDetailComponent implements OnInit {
+  albumId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParametersArray) => {
+      this.albumId = parseInt(urlParametersArray['id']);
+    })
   }
 
 }
